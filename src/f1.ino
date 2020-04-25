23428744464192d93ee7f8f26751fea44431c97f
Jeroen Bourgois <jeroenbourgois@gmail.com>
2020-04-25 18:04:33 +0200

    update

--- src/f1.ino
+++ src/f1.ino
@@ -1,63 +1,64 @@
-/*
- * F1
- *
- * Produces a full F1 startgrid sequence.
- *
- * Developed for the LSST Data Management System.
- * This product includes software developed by the LSST Project
- * (https://www.lsst.org).
- * See the COPYRIGHT file at the top-level directory of this distribution
- * for details of code ownership.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
- * Circuit:
- * - 8 ohm speaker on digital pin 8
- *
- * The circuit:
- * - LED attached from pin 13 to ground
- * - pushbutton attached to pin 2 from +5V
- * - 10K resistor attached to pin 2 from ground
- *
- * created 1 Ap 2020
- * modified 1 Ap 2020
- * by defborn
- *
- * This code is in the public domain.
- *
- * TODO:
- * - use F() macro when printing
- */
+// F1
+//
+// Produces a full F1 startgrid sequence.
+//
+// Developed for the LSST Data Management System.
+// This product includes software developed by the LSST Project
+// (https://www.lsst.org).
+// See the COPYRIGHT file at the top-level directory of this distribution
+// for details of code ownership.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// Circuit:
+// - 8 ohm speaker on digital pin 8
+// - two photo meters
+// - 5 LEDs
+//
+// The circuit:
+// - LED attached from pin 13 to ground
+// - pushbutton attached to pin 2 from +5V
+// - 10K resistor attached to pin 2 from ground
+//
+// Created:  April 1st, 2020
+// Modified: April 25, 2020
+// By Jeroen Bourgois.
+//
+// This code is in the public domain.
+//
+// TODO:
+// - use F() macro when printing
 
 #include "pitches.h"
 #include <LiquidCrystal.h>
 
-const int PIN_BUZZER = 12;
-const int PIN_BTN = A5;
-const int PIN_LCD_RS = 3;
-const int PIN_LCD_E = 8;
-const int PIN_LCD_D4 = 7;
-const int PIN_LCD_D5 = 9;
-const int PIN_LCD_D6 = 10;
-const int PIN_LCD_D7 = 11;
-const int NUM_LIGHTS = 5;
-const int PIN_LIGHTS[NUM_LIGHTS] = {2, 13, 4, 5, 6};
-const int NOTE_DURATION_SHORT = 500;
-const int NOTE_DURATION_LONG = 1500;
-const int NOTE_DELAY = 1000;
-const int PIN_P1_SENSOR = A0;
-const int PIN_P2_SENSOR = A1;
+const int pinBuzzer = 12;
+const int pinBtnConfirm = A5;
+const int pinBtnCancel = A4;
+const int pinLCD_RS = 3;
+const int pinLCD_E = 8;
+const int pinLCD_D4 = 7;
+const int pinLCD_D5 = 9;
+const int pinLCD_D6 = 10;
+const int pinLCD_D7 = 11;
+const int numLights = 5;
+const int pinLights[numLights] = {2, 13, 4, 5, 6};
+const int noteDurationShort = 500;
+const int noteDurationLong = 1500;
+const int noteDelay = 1000;
+const int pinP1Sensor = A0;
+const int pinP2Sensor = A1;
 
 char p1Name[4] = "HAM";
 unsigned long p1PrevLap = 0;
@@ -67,14 +68,14 @@ char p2Name[4] = "VET";
 unsigned long p2PrevLap = 0;
 unsigned long p2CurLap = 0;
 unsigned long p2BestLap = 0;
-const char PLAYERS_PAD[11] =      "..........";
-const char PLAYER_1_FASTEST[11] = " <-       ";
-const char PLAYER_2_FASTEST[11] = ".......->.";
-const int DISPLAY_START = 0;
-const int DISPLAY_COUNTDOWN_START = 1;
-const int DISPLAY_COUNTDOWN_GO = 2;
-const int DISPLAY_RACE = 3;
-const int DISPLAY_FINISH = 4;
+const char playersPad[11] =      "..........";
+const char player1Fastest[11] = " <-       ";
+const char player2Fastest[11] = ".......->.";
+const int displayStart = 0;
+const int displayCountdownStart = 1;
+const int displayCountdownGo = 2;
+const int displayRace = 3;
+const int displayFinish = 4;
 int displayState = 0;
 float sensorP1Value = 0.0;
 float sensorP2Value = 0.0;
@@ -85,8 +86,8 @@ char _s[3];
 char _ms[3];
 char* _mls = new char[12];
 
-const int BLOCK_POS[5] = {1, 4, 7, 10, 13};
-const byte FULL_BLOCK[] = {
+const int blockPos[5] = {1, 4, 7, 10, 13};
+const byte fullBlock[] = {
   B11111,
   B11111,
   B11111,
@@ -97,39 +98,38 @@ const byte FULL_BLOCK[] = {
   B11111
 };
 
-LiquidCrystal lcd(PIN_LCD_RS, PIN_LCD_E, PIN_LCD_D4, PIN_LCD_D5, PIN_LCD_D6, PIN_LCD_D7);
+LiquidCrystal lcd(pinLCD_RS, pinLCD_E, pinLCD_D4, pinLCD_D5, pinLCD_D6, pinLCD_D7);
 
-int lastButtonState = LOW;
-// In F1, the time between all lights on
-// and all lights out (= GO GO GO!) is
+int lastConfirmBtnState = LOW;
+int lastCancelBtnState = LOW;
+// In F1, the time between all lights on and all lights out is
 // a random delay between 1 and 5 seconds.
 int randomStartDelay;
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.createChar(0, FULL_BLOCK);
+  lcd.createChar(0, fullBlock);
   initLights();
   Serial.begin(9600);
 }
 
 void initLights() {
   randomStartDelay = random(1000, 5000);
-  for (int i = 0; i < NUM_LIGHTS; i++) {
-    pinMode(PIN_LIGHTS[i], OUTPUT);
+  for (int i = 0; i < numLights; i++) {
+    pinMode(pinLights[i], OUTPUT);
   }
-  pinMode(PIN_BTN, INPUT);
+  pinMode(pinBtnConfirm, INPUT);
 }
 
 void loop() {
-  checkMainBtn();
-  checkSensor();
+  checkButtons();
+  checkSensors();
   updateLapTimer();
   drawDisplay();
 }
 
-/*
-* byte(0) is a full block
-*/
+// Used to print a solid block on the LCD
+// byte(0) is a full block
 void writeFullBlock(int n) {
   lcd.setCursor(n, 0);
   lcd.write(byte(0));
@@ -143,68 +143,79 @@ void writeFullBlock(int n) {
 
 void playSequence() {
   lcd.clear();
-  delay(NOTE_DELAY);
+  delay(noteDelay);
 
-  writeFullBlock(BLOCK_POS[0]);
+  writeFullBlock(blockPos[0]);
 
-  tone(PIN_BUZZER, NOTE_C5, NOTE_DURATION_SHORT);
-  digitalWrite(PIN_LIGHTS[0], HIGH);
-  delay(NOTE_DELAY);
+  tone(pinBuzzer, NOTE_C5, noteDurationShort);
+  digitalWrite(pinLights[0], HIGH);
+  delay(noteDelay);
 
-  writeFullBlock(BLOCK_POS[1]);
+  writeFullBlock(blockPos[1]);
 
-  tone(PIN_BUZZER, NOTE_C5, NOTE_DURATION_SHORT);
-  digitalWrite(PIN_LIGHTS[1], HIGH);
-  delay(NOTE_DELAY);
+  tone(pinBuzzer, NOTE_C5, noteDurationShort);
+  digitalWrite(pinLights[1], HIGH);
+  delay(noteDelay);
 
-  writeFullBlock(BLOCK_POS[2]);
+  writeFullBlock(blockPos[2]);
 
-  tone(PIN_BUZZER, NOTE_C5, NOTE_DURATION_SHORT);
-  digitalWrite(PIN_LIGHTS[2], HIGH);
-  delay(NOTE_DELAY);
+  tone(pinBuzzer, NOTE_C5, noteDurationShort);
+  digitalWrite(pinLights[2], HIGH);
+  delay(noteDelay);
 
-  writeFullBlock(BLOCK_POS[3]);
+  writeFullBlock(blockPos[3]);
 
-  tone(PIN_BUZZER, NOTE_C5, NOTE_DURATION_SHORT);
-  digitalWrite(PIN_LIGHTS[3], HIGH);
-  delay(NOTE_DELAY);
+  tone(pinBuzzer, NOTE_C5, noteDurationShort);
+  digitalWrite(pinLights[3], HIGH);
+  delay(noteDelay);
 
-  writeFullBlock(BLOCK_POS[4]);
+  writeFullBlock(blockPos[4]);
 
-  tone(PIN_BUZZER, NOTE_C5, NOTE_DURATION_SHORT);
-  digitalWrite(PIN_LIGHTS[4], HIGH);
+  tone(pinBuzzer, NOTE_C5, noteDurationShort);
+  digitalWrite(pinLights[4], HIGH);
   delay(randomStartDelay);
 
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("  Go! Go! Go! ");
 
-  tone(PIN_BUZZER, NOTE_G5, NOTE_DURATION_LONG);
+  tone(pinBuzzer, NOTE_G5, noteDurationLong);
   lightsOut();
 
-  delay(NOTE_DELAY);
-  noTone(PIN_BUZZER);
+  delay(noteDelay);
+  noTone(pinBuzzer);
 
   startRace();
 }
 
-/**
-   Read the main button state
+// Depending on the screen, handle the buttons
+// Confirm button to start / ok / continue
+// Cancel button to cancel / back / stop
+void checkButtons() {
+  checkConfirmButton();
+  checkCancelButton();
+}
 
-   If the button is pressed (compared to last state)
-   we (re)start the sequence.
-   Analog read!
-*/
-void checkMainBtn() {
-  int buttonState = digitalRead(PIN_BTN);
-  if (buttonState != lastButtonState) {
-    if (buttonState == HIGH) {
+void checkConfirmButton() {
+  int state = digitalRead(pinBtnConfirm);
+  if (state != lastConfirmBtnState) {
+    if (state == HIGH) {
       startSequence();
-      // startRace();
     }
     delay(50);
+    lastConfirmBtnState = state;
   }
-  lastButtonState = buttonState;
+}
+
+void checkCancelButton() {
+  int state = digitalRead(pinBtnCancel);
+  if (state != lastCancelBtnState) {
+    if (state == HIGH) {
+      startSequence();
+    }
+    delay(50);
+    lastCancelBtnState = state;
+  }
 }
 
 void startSequence() {
@@ -217,14 +228,15 @@ void resetSequence() {
 }
 
 void lightsOut() {
-  for (int i = 0; i < NUM_LIGHTS; i++) {
-    digitalWrite(PIN_LIGHTS[i], LOW);
+  for (int i = 0; i < numLights; i++) {
+    digitalWrite(pinLights[i], LOW);
   }
 }
 
 // Controleer de waarde van de lichtsensor
-void checkSensor() {
-  sensorP1Value = analogRead(PIN_P1_SENSOR);
+void checkSensors() {
+  checkSensor(pinP1Sensor, curLap)
+  sensorP1Value = analogRead(pinP1Sensor);
   if (sensorP1Value > 100 && sensorP1Value < 200) {
     if (p1CurLap < p1BestLap) {
       p1BestLap = p1CurLap;
@@ -234,15 +246,16 @@ void checkSensor() {
     // }
     p1PrevLap = millis();
   }
-  sensorP2Value = analogRead(PIN_P1_SENSOR);
-  if (sensorP2Value > 100 && sensorP2Value < 200) {
-    if (p1CurLap < p1BestLap) {
-      p1BestLap = p1CurLap;
+
+}
+
+void checkSensor(int sensorPin, unsigned long *curLap, unsigned long *bestLap, unsigned long *prevLap) {
+  unsigned long val = analogRead(sensorPin);
+  if (val > 100 && val < 200) {
+    if (curLap < bestLap) {
+      *bestLap = &curLap;
     }
-    // if (p2CurLap < p2BestLap || p2BestLap == 0) {
-    //   p2BestLap = p2CurLap;
-    // }
-    p2PrevLap = millis();
+    *prevLap = millis();
   }
 }
 
@@ -253,13 +266,13 @@ void updateLapTimer() {
 }
 
 void startRace() {
-  displayState = DISPLAY_RACE;
+  displayState = displayRace;
   // p1PrevLap = p1CurLap = p2PrevLap = p2CurLap = millis();
 }
 
 void drawDisplay() {
   switch (displayState) {
-    case DISPLAY_START:
+    case displayStart:
       Serial.println("START DISPLAY");
       lcd.setCursor(0, 0);
       lcd.write("*  FORMULA ONE * ");
@@ -267,7 +280,7 @@ void drawDisplay() {
       lcd.write(" -> press start ");
       break;
 
-    case DISPLAY_RACE:
+    case displayRace:
       // Serial.println("RACE DISPLAY");
       p1Time = millisToString(p1CurLap);
       p2Time = millisToString(p2CurLap);
@@ -284,7 +297,7 @@ void drawDisplay() {
       // } else {
       //   strcat(line1, PLAYER_2_FASTEST);
       // }
-      strcat(line1, PLAYER_1_FASTEST);
+      strcat(line1, player1Fastest);
       strcat(line1, p2Name);
 
       char line2[17];
@@ -312,3 +325,4 @@ char * millisToString(unsigned long millis) {
   sprintf(_mls, "%0d:%02d:%02d", m, s, ms);
   return _mls;
 }
+
