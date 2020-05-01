ae9cb2d805084976dcd3424e5f6db1076c9f56f1
Jeroen Bourgois <jeroenbourgois@gmail.com>
2020-05-01 14:35:50 +0200

    update

--- src/f1.ino
+++ src/f1.ino
@@ -25,7 +25,7 @@
 // - 8 ohm speaker on digital pin 8
 // - two photo meters
 // - 5 LEDs
-//
+// - 10k resistor for phototransistor
 // The circuit:
 // - LED attached from pin 13 to ground
 // - pushbutton attached to pin 2 from +5V
@@ -43,8 +43,9 @@
 #include "pitches.h"
 #include <LiquidCrystal.h>
 
+// Board pins
 const int pinBuzzer = 12;
-const int pinBtnConfirm = A5;
+const int pinBtnConfirm = A3;
 const int pinBtnCancel = A4;
 const int pinLCD_RS = 3;
 const int pinLCD_E = 8;
@@ -54,37 +55,34 @@ const int pinLCD_D6 = 10;
 const int pinLCD_D7 = 11;
 const int numLights = 5;
 const int pinLights[numLights] = {2, 13, 4, 5, 6};
+const int pinP1Sensor = A1;
+const int pinP2Sensor = A2;
+
 const int noteDurationShort = 500;
 const int noteDurationLong = 1500;
 const int noteDelay = 1000;
-const int pinP1Sensor = A0;
-const int pinP2Sensor = A1;
-
 char p1Name[4] = "HAM";
 unsigned long p1PrevLap = 0;
 unsigned long p1CurLap = 0;
-unsigned long p1BestLap = 0;
+unsigned long p1BestLap = 9999999;
 char p2Name[4] = "VET";
 unsigned long p2PrevLap = 0;
 unsigned long p2CurLap = 0;
-unsigned long p2BestLap = 0;
-const char playersPad[11] =      "..........";
+unsigned long p2BestLap = 9999999;
+const char playersPad[11] =     "          ";
 const char player1Fastest[11] = " <-       ";
-const char player2Fastest[11] = ".......->.";
-const int displayStart = 0;
-const int displayCountdownStart = 1;
-const int displayCountdownGo = 2;
-const int displayRace = 3;
-const int displayFinish = 4;
-int displayState = 0;
-float sensorP1Value = 0.0;
-float sensorP2Value = 0.0;
-char* p1Time = new char[9];
-char* p2Time = new char[9];
-char _m[3];
-char _s[3];
-char _ms[3];
-char* _mls = new char[12];
+const char player2Fastest[11] = "       -> ";
+
+// screens
+const int startScreen = 0;
+const int countdownScreen = 1;
+const int countdownGoScreen = 2;
+const int raceScreen = 3;
+const int finishScreen = 4;
+int activeScreen = -1;
+int nextScreen = 0;
+
+// reed switch
 
 const int blockPos[5] = {1, 4, 7, 10, 13};
 const byte fullBlock[] = {
@@ -102,30 +100,42 @@ LiquidCrystal lcd(pinLCD_RS, pinLCD_E, pinLCD_D4, pinLCD_D5, pinLCD_D6, pinLCD_D
 
 int lastConfirmBtnState = LOW;
 int lastCancelBtnState = LOW;
-// In F1, the time between all lights on and all lights out is
-// a random delay between 1 and 5 seconds.
+// In F1, the time between all lights on and all lights out
+// is a random delay. We take something between 1 and 5 seconds.
 int randomStartDelay;
 
 void setup() {
-  lcd.begin(16, 2);
-  lcd.createChar(0, fullBlock);
-  initLights();
+  setupLCD();
+  setupLights();
+  setupButtons();
   Serial.begin(9600);
-}
-
-void initLights() {
-  randomStartDelay = random(1000, 5000);
-  for (int i = 0; i < numLights; i++) {
-    pinMode(pinLights[i], OUTPUT);
-  }
-  pinMode(pinBtnConfirm, INPUT);
 }
 
 void loop() {
   checkButtons();
   checkSensors();
   updateLapTimer();
+  updateGameState();
   drawDisplay();
+}
+
+void setupLights() {
+  randomStartDelay = random(1000, 5000);
+  for (int i = 0; i < numLights; i++) {
+    pinMode(pinLights[i], OUTPUT);
+  }
+}
+
+void setupLCD() {
+  lcd.begin(16, 2);
+  lcd.createChar(0, fullBlock);
+}
+
+void setupButtons() {
+  pinMode(pinBtnConfirm, INPUT);
+  pinMode(pinBtnCancel, INPUT);
+  pinMode(pinP1Sensor, INPUT);
+  pinMode(pinP2Sensor, INPUT);
 }
 
 // Used to print a solid block on the LCD
@@ -133,17 +143,19 @@ void loop() {
 void writeFullBlock(int n) {
   lcd.setCursor(n, 0);
   lcd.write(byte(0));
-  lcd.setCursor(n+1, 0);
+  lcd.setCursor(n + 1, 0);
   lcd.write(byte(0));
   lcd.setCursor(n, 1);
   lcd.write(byte(0));
-  lcd.setCursor(n+1, 1);
+  lcd.setCursor(n + 1, 1);
   lcd.write(byte(0));
 }
 
 void playSequence() {
-  lcd.clear();
+  delay(10000);
   delay(noteDelay);
+
+  lcd.clear();
 
   writeFullBlock(blockPos[0]);
 
@@ -175,47 +187,64 @@ void playSequence() {
   digitalWrite(pinLights[4], HIGH);
   delay(randomStartDelay);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("  Go! Go! Go! ");
+  setScreen(countdownGoScreen);
+  updateGameState();
 
   tone(pinBuzzer, NOTE_G5, noteDurationLong);
+
   lightsOut();
+  setScreen(raceScreen);
 
   delay(noteDelay);
   noTone(pinBuzzer);
-
-  startRace();
 }
 
 // Depending on the screen, handle the buttons
 // Confirm button to start / ok / continue
 // Cancel button to cancel / back / stop
 void checkButtons() {
-  checkConfirmButton();
-  checkCancelButton();
+  lastConfirmBtnState = checkButton(pinBtnConfirm, lastConfirmBtnState, 1);
+  lastCancelBtnState = checkButton(pinBtnCancel, lastCancelBtnState, -1);
 }
 
-void checkConfirmButton() {
-  int state = digitalRead(pinBtnConfirm);
-  if (state != lastConfirmBtnState) {
+int checkButton(int pin, int lastState, int screenBump) {
+  int state = digitalRead(pin);
+  if (state != lastState) {
     if (state == HIGH) {
-      startSequence();
+      setScreen(activeScreen + screenBump);
     }
     delay(50);
-    lastConfirmBtnState = state;
+    return state;
   }
+  return lastState;
 }
 
-void checkCancelButton() {
-  int state = digitalRead(pinBtnCancel);
-  if (state != lastCancelBtnState) {
-    if (state == HIGH) {
-      startSequence();
-    }
-    delay(50);
-    lastCancelBtnState = state;
+// This drives what happens in the
+// setup. Based on the next screen, it will
+// perform the needed actions to put the game
+// in the next state
+void updateGameState() {
+  if (nextScreen == activeScreen) {
+    return;
   }
+  Serial.print(" A: ");
+  Serial.print(activeScreen);
+  Serial.print(" | N: ");
+  Serial.print(nextScreen);
+  Serial.println();
+  activeScreen = nextScreen;
+  switch (nextScreen) {
+    case countdownScreen:
+      drawDisplay();
+      startSequence();
+      break;
+    case raceScreen:
+      startRace();
+    case countdownGoScreen:
+      drawDisplay();
+      break;
+  }
+
 }
 
 void startSequence() {
@@ -233,78 +262,53 @@ void lightsOut() {
   }
 }
 
-// Controleer de waarde van de lichtsensor
-void checkSensors() {
-  checkSensor(pinP1Sensor, curLap)
-  sensorP1Value = analogRead(pinP1Sensor);
-  if (sensorP1Value > 100 && sensorP1Value < 200) {
-    if (p1CurLap < p1BestLap) {
-      p1BestLap = p1CurLap;
-    }
-    // if (p2CurLap < p2BestLap || p2BestLap == 0) {
-    //   p2BestLap = p2CurLap;
-    // }
-    p1PrevLap = millis();
-  }
-
-}
-
-void checkSensor(int sensorPin, unsigned long *curLap, unsigned long *bestLap, unsigned long *prevLap) {
-  unsigned long val = analogRead(sensorPin);
-  if (val > 100 && val < 200) {
-    if (curLap < bestLap) {
-      *bestLap = &curLap;
-    }
-    *prevLap = millis();
-  }
-}
-
-void updateLapTimer() {
-  unsigned long timestamp = millis();
-  p1CurLap = timestamp - p1PrevLap;
-  // p2CurLap = timestamp - p2PrevLap;
-}
-
 void startRace() {
-  displayState = displayRace;
-  // p1PrevLap = p1CurLap = p2PrevLap = p2CurLap = millis();
+  unsigned long ms = millis();
+  p1PrevLap = ms;
+  p1CurLap = ms;
+  p2PrevLap = ms;
+  p2CurLap = ms;
 }
 
 void drawDisplay() {
-  switch (displayState) {
-    case displayStart:
-      Serial.println("START DISPLAY");
+  switch (activeScreen) {
+    case startScreen:
       lcd.setCursor(0, 0);
-      lcd.write("*  FORMULA ONE * ");
+      lcd.write("*  FORMULA ONE *");
       lcd.setCursor(0, 1);
-      lcd.write(" -> press start ");
+      lcd.write("*  PRESS START *");
       break;
-
-    case displayRace:
-      // Serial.println("RACE DISPLAY");
-      p1Time = millisToString(p1CurLap);
-      p2Time = millisToString(p2CurLap);
-      // char p1Time[8] = "0:00:00";
-      // char p2Time[8] = "0:00:00";
-      //       Serial.println(p1CurLap);
-      // Serial.println(p1Time);
+    case countdownScreen:
+      lcd.setCursor(0, 0);
+      lcd.write("   10 SEC UNTIL ");
+      lcd.setCursor(0, 1);
+      lcd.write("    LIGHTS OUT  ");
+      break;
+    case countdownGoScreen:
+      lcd.setCursor(0, 0);
+      lcd.print(" It's lights out");
+      lcd.setCursor(0, 1);
+      lcd.print(" and away we go!");
+      break;
+    case raceScreen:
+      char p1Time[9] = {0};
+      char p2Time[9] = {0};
+      millisToString(p1CurLap, p1Time);
+      millisToString(p2CurLap, p2Time);
       char line1[17];
       strcpy(line1, p1Name);
-      // if (p1BestLap == p2BestLap) {
-      //   strcat(line1, PLAYERS_PAD);
-      // } else if (p1BestLap < p2BestLap) {
-      //   strcat(line1, PLAYER_1_FASTEST);
-      // } else {
-      //   strcat(line1, PLAYER_2_FASTEST);
-      // }
-      strcat(line1, player1Fastest);
+      if (p1BestLap == p2BestLap) {
+        strcat(line1, playersPad);
+      } else if (p1BestLap < p2BestLap) {
+        strcat(line1, player1Fastest);
+      } else {
+        strcat(line1, player2Fastest);
+      }
       strcat(line1, p2Name);
-
       char line2[17];
       strcpy(line2, p1Time);
       strcat(line2, "  ");
       strcat(line2, p2Time);
-
       lcd.setCursor(0, 0);
       lcd.write(line1);
       lcd.setCursor(0, 1);
@@ -313,16 +317,48 @@ void drawDisplay() {
   }
 }
 
-char * millisToString(unsigned long millis) {
-  // bit dirty?
-  if (millis == 0) {
-    return "0:00:00";
+void setScreen(int screen) {
+  nextScreen = screen;
+  if(nextScreen < 0) {
+    nextScreen = 0;
   }
-  // char* str = new char[12];
+  Serial.print("setScreen(");
+  Serial.print(screen);
+  Serial.print(")");
+  Serial.println();
+}
+
+// Check the reed sensor
+// https://en.wikipedia.org/wiki/Reed_switch
+// For now (no reed sensors yet) we stubbed with a push button
+void checkSensors() {
+  checkSensor(pinP1Sensor, p1CurLap, p1PrevLap, p1BestLap);
+  checkSensor(pinP2Sensor, p2CurLap, p2PrevLap, p2BestLap);
+}
+
+void checkSensor(int pin, unsigned long &curLap, unsigned long &prevLap, unsigned long &bestLap) {
+  // unsigned long val = analogRead(pin);
+  int val = digitalRead(pin);
+  if (val == HIGH) {
+    if (curLap < bestLap && curLap > 1000) {
+      bestLap = curLap;
+    }
+    prevLap = millis();
+  }
+}
+
+void updateLapTimer() {
+  unsigned long timestamp = millis();
+  p1CurLap = timestamp - p1PrevLap;
+  p2CurLap = timestamp - p2PrevLap;
+}
+
+
+char * millisToString(unsigned long millis, char *out) {
   int s = (millis / 1000) % 60;
   int m = (millis / 1000) / 60;
   int ms = (millis % 1000) / 10;
-  sprintf(_mls, "%0d:%02d:%02d", m, s, ms);
-  return _mls;
+  sprintf(out, "%0d:%02d:%02d", m, s, ms);
+  return out;
 }
-
+
---
+++ test/f1.cpp
@@ -0,0 +1,67 @@
+// Just a testfile to tryout some of the methods used
+// in the Arduino sketch. It was faster doing it
+// in C++ and test with the compiler/cli
+#include<iostream>
+
+unsigned long millis() {
+  return 1000.0;
+}
+
+void checkSensor(int sensorPin, unsigned long &curLap, unsigned long &bestLap, unsigned long &prevLap) {
+  unsigned long val = 5000; //analogRead(sensorPin);
+  prevLap = 1234.0;
+  curLap = 20.0;
+  if (val > 100 && val < 200) {
+    if (curLap < bestLap) {
+      bestLap = curLap;
+    }
+    prevLap = millis();
+  }
+}
+
+
+char* _mls = new char[12];
+
+/* char * millisToString(unsigned long millis) { */
+/*   char* str = new char[12]; */
+/*   int s = (millis / 1000) % 60; */
+/*   int m = (millis / 1000) / 60; */
+/*   int ms = (millis % 1000) / 10; */
+/*   sprintf(str, "%0d:%02d:%02d", m, s, ms); */
+/*   return str; */
+/* } */
+
+char * millisToString(unsigned long millis, char *out) {
+  int s = (millis / 1000) % 60;
+  int m = (millis / 1000) / 60;
+  int ms = (millis % 1000) / 10;
+  sprintf(out, "%0d:%02d:%02d", m, s, ms);
+  return out;
+}
+
+int main () {
+  int sensorPin = 1;
+  unsigned long curLap = 1000;
+  unsigned long bestLap = 1000;
+  unsigned long prevLap = 1000;
+
+  /* std::cout << curLap << std::endl; */
+  /* std::cout << bestLap << std::endl; */
+  /* std::cout << prevLap << std::endl; */
+  /* std::cout << "-----" << std::endl; */
+  /* checkSensor(sensorPin, curLap, bestLap, prevLap); */
+  /* std::cout << curLap << std::endl; */
+  /* std::cout << bestLap << std::endl; */
+  /* std::cout << prevLap << std::endl; */
+  /* std::cout << "-----" << std::endl; */
+  /* char p1Time[9] = {0}; */
+  char *p1Time[9] = {0};
+  millisToString(1500, p1Time);
+  std::cout << p1Time << std::endl;
+  /* const char* p2Time = millisToString(2500); */
+  /* std::cout << p2Time << std::endl; */
+  /* p1Time = millisToString(1500); */
+  /* std::cout << p1Time << std::endl; */
+  /* p2Time = millisToString(0); */
+  /* std::cout << p2Time << std::endl; */
+}
